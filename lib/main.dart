c9236dc23c5c4636149acb38c33fbb644e8ea4e4
paivalucas <lucas@pagow.app>
2020-04-02 22:42:05 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.white,
+        accentColor: Colors.white,
+      ),
+      home: MyHomePage(title: 'Login Pagina'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final formKey = GlobalKey<FormState>();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  String _email;
+  String _password;
+
+  void _submitCommand() {
+    final form = formKey.currentState;
+
+    if (form.validate()) {
+      form.save();
+
+      // Email & password matched our validation rules
+      // and are saved to _email and _password fields.
+      _loginCommand();
+    }
+  }
+
+  void _loginCommand() {
+    // This is just a demo, so no actual login here.
+    final snackbar = SnackBar(
+      content: Text('Email: $_email, password: $_password'),
+    );
+
+    scaffoldKey.currentState.showSnackBar(snackbar);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xFFFF7255),
+        key: scaffoldKey,
+        body: Theme(
+          data: Theme.of(context).copyWith(accentColor: Colors.white),
+          child: Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Olá novamente!",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40.0,
+                            color: Colors.white),
+                      )
+                    ],
+                  ),
+                  Divider(
+                    color: Color(0xFFFF7255),
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.email,
+                        color: Colors.white,
+                      ),
+                      hintText: 'Insira seu email',
+                      labelText: 'Endereço de Email',
+                    ),
+                    style: TextStyle(color: Colors.white),
+                    validator: (val) => !EmailValidator.validate(val, true)
+                        ? 'Email inválido.'
+                        : null,
+                    onSaved: (val) => _email = val,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.lock,
+                          color: Colors.white,
+                        ),
+                        hintText: 'Insira sua senha',
+                        focusColor: Colors.white,
+                        labelText: 'Senha'),
+                    validator: (val) =>
+                        val.length < 4 ? 'Senha inválida' : null,
+                    onSaved: (val) => _password = val,
+                    obscureText: true,
+                  ),
+                  Divider(
+                    color: Color(0xFFFF7255),
+                    height: 30.0,
+                  ),
+                  ConstrainedBox(
+                      constraints:
+                          const BoxConstraints(minWidth: double.infinity),
+                      child: RaisedButton(
+                          shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(30.0)),
+                          padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
+                          onPressed: _submitCommand,
+                          color: Colors.white,
+                          child: Text(
+                            'Entrar',
+                            style: TextStyle(
+                              fontSize: 17.0,
+                              color: Color(0xFFFF7255),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ))),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
