d1bef473aa6d071cf074e7d097626e4930d324e3
paivalucas <lucas@pagow.app>
2020-04-05 10:45:42 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Login',
       theme: ThemeData(
+        ///
+        /// tema app
+        ///
         brightness: Brightness.dark,
         primaryColor: Colors.white,
         accentColor: Colors.white,
@@ -29,6 +32,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final formKey = GlobalKey<FormState>();
+
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
   String _email;
@@ -53,15 +57,21 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+
+  ///
+  ///
+  ///  Tela Login
+  ///
+  ///
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFFF7255),
       key: scaffoldKey,
       body: SingleChildScrollView(
-        padding: const EdgeInsets.all(40.0),
+        padding: const EdgeInsets.fromLTRB(40, 60, 40, 0),
         child: Column(
           children: <Widget>[
-            Row(              
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Image.asset('images/logo.png', width: 120.0),
@@ -89,61 +99,78 @@ class _MyHomePageState extends State<MyHomePage> {
                   color: Color(0xFFFF7255),
                   height: 20.0,
                 ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    icon: Icon(
-                      Icons.email,
-                      color: Colors.white,
-                    ),
-                    hintText: 'Insira seu email',
-                    labelText: 'Endereço de Email',
-                  ),
-                  style: TextStyle(color: Colors.white),
-                  validator: (val) => !EmailValidator.validate(val, true)
-                      ? 'Email inválido.'
-                      : null,
-                  onSaved: (val) => _email = val,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                      icon: Icon(
-                        Icons.lock,
-                        color: Colors.white,
-                      ),
-                      hintText: 'Insira sua senha',
-                      focusColor: Colors.white,
-                      labelText: 'Senha'),
-                  validator: (val) => val.length < 4 ? 'Senha inválida' : null,
-                  onSaved: (val) => _password = val,
-                  obscureText: true,
-                ),
-                Divider(
-                  color: Color(0xFFFF7255),
-                  height: 30.0,
-                ),
-                ConstrainedBox(
-                    constraints:
-                        const BoxConstraints(minWidth: double.infinity),
-                    child: RaisedButton(
-                        shape: new RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(30.0)),
-                        padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
-                        onPressed: () {},
-                        color: Colors.white,
-                        child: Text(
-                          'Entrar',
-                          style: TextStyle(
-                            fontSize: 17.0,
-                            color: Color(0xFFFF7255),
-                            fontWeight: FontWeight.bold,
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(
+                          icon: Icon(
+                            Icons.email,
+                            color: Colors.white,
                           ),
-                        ))),
+                          hintText: 'Insira seu email',
+                          labelText: 'Endereço de Email',
+                        ),
+                        style: TextStyle(color: Colors.white),
+                        validator: (val) => !EmailValidator.validate(val, true)
+                            ? 'Email inválido.'
+                            : null,
+                        onSaved: (val) => _email = val,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            icon: Icon(
+                              Icons.lock,
+                              color: Colors.white,
+                            ),
+                            hintText: 'Insira sua senha',
+                            focusColor: Colors.white,
+                            labelText: 'Senha'),
+                        validator: (val) =>
+                            val.length < 4 ? 'Senha inválida' : null,
+                        onSaved: (val) => _password = val,
+                        obscureText: true,
+                      ),
+                      Divider(
+                        color: Color(0xFFFF7255),
+                        height: 30.0,
+                      ),
+                      ConstrainedBox(
+                          constraints:
+                              const BoxConstraints(minWidth: double.infinity),
+                          child: RaisedButton(
+                              shape: new RoundedRectangleBorder(
+                                  borderRadius:
+                                      new BorderRadius.circular(30.0)),
+                              padding:
+                                  EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
+                              onPressed: () {
+                                _submitCommand();
+                              },
+                              color: Colors.white,
+                              child: Text(
+                                'Entrar',
+                                style: TextStyle(
+                                  fontSize: 17.0,
+                                  color: Color(0xFFFF7255),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )))
+                    ],
+                  ),
+                ),
                 Divider(
                   color: Color(0xFFFF7255),
                   height: 20.0,
                 ),
                 FlatButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => EsqueciSenha()));
+                  },
                   child: Text(
                     'Esqueceu a senha?',
                     style: TextStyle(fontSize: 17),
@@ -165,14 +192,154 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+/* 
+navigator onPress(){~~~}
+    Navigator.push(
+    context,
+     MaterialPageRoute(
+     builder: (context) => TelaPrincipal()),
+);                        
+*/
+///
+///
+///
+///  Tela Esqueci a senha
+///
+///
+///
+class EsqueciSenha extends StatefulWidget {
+  @override
+  _EsqueciSenhaState createState() => _EsqueciSenhaState();
+}
 
-/* cmd pro onPress(){}
+class _EsqueciSenhaState extends State<EsqueciSenha> {
 
 
-                          Navigator.push(
+  _submitEsqueci() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF333333),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.fromLTRB(40, 60, 40, 40),
+        child: Column(
+          children: <Widget>[
+            Row(
+              //cabecalho
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    // coluna 1/3
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => TelaPrincipal()),
-                          );
-                        
-*/
+                                builder: (context) => MyHomePage()));
+                      },
+                      child: Icon(Icons.arrow_back, color: Colors.white),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    //coluna 2/3
+                    Image.asset('images/logo.png', width: 120.0),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    //coluna 3/3
+                  ],
+                )
+              ],
+            ),
+            ///
+            /// Fim cabecalho
+            ///
+            Divider(
+              color: Color(0xFF333333),
+              height: 160,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Recuperar senha",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 28.0,
+                      color: Colors.white),
+                ),
+              ],
+            ),
+            Text(
+              "Insira o email da sua conta para que possamos te ajudar a recuperar",
+              style: TextStyle(
+                  fontWeight: FontWeight.w100,
+                  fontSize: 20.0,
+                  color: Colors.white),
+            ),
+            Divider(color: Color(0xFF33333), height: 40.0),
+            TextFormField(
+              decoration: InputDecoration(
+                icon: Icon(
+                  Icons.email,
+                  color: Colors.white,
+                ),
+                hintText: 'Insira seu email',
+                labelText: 'Endereço de Email',
+              ),
+              style: TextStyle(color: Colors.white),
+              validator: (val) => !EmailValidator.validate(val, true)
+                  ? 'Email inválido.'
+                  : null,
+            ),
+            Divider(color: Color(0xFF33333), height: 40.0),
+            ConstrainedBox(
+                constraints: const BoxConstraints(minWidth: double.infinity),
+                child: RaisedButton(
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30.0)),
+                    padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
+                    onPressed: () {
+                      _submitEsqueci();
+                    },
+                    color: Colors.white,
+                    child: Text(
+                      'Enviar',
+                      style: TextStyle(
+                        fontSize: 17.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+///
+///
+///
+/// Tela Cadastro
+/// 
+/// 
+/// 
+
+class TelaCadastro extends StatefulWidget {
+  @override
+  _TelaCadastroState createState() => _TelaCadastroState();
+}
+
+class _TelaCadastroState extends State<TelaCadastro> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
