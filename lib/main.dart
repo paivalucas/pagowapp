a8e24ef6cb572becc9557da99658410dfbf9e1a3
paivalucas <lucas@pagow.app>
2020-04-06 16:29:54 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -177,7 +177,12 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                 FlatButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => TelaCadastro()));
+                  },
                   child: Text(
                     'Crie uma conta agora',
                     style: TextStyle(fontSize: 17),
@@ -213,9 +218,28 @@ class EsqueciSenha extends StatefulWidget {
 }
 
 class _EsqueciSenhaState extends State<EsqueciSenha> {
+  final formKey = GlobalKey<FormState>();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
 
+  String _emailEsqueci;
 
-  _submitEsqueci() {}
+  void _submitEsqueci() {
+    final form = formKey.currentState;
+
+    if (form.validate()) {
+      form.save();
+
+      _esqueciComando();
+    }
+  }
+
+  void _esqueciComando() {
+    final snackbar = SnackBar(
+      content: Text('Email: $_emailEsqueci'),
+    );
+
+    scaffoldKey.currentState.showSnackBar(snackbar);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -255,6 +279,7 @@ class _EsqueciSenhaState extends State<EsqueciSenha> {
                 )
               ],
             ),
+
             ///
             /// Fim cabecalho
             ///
@@ -326,9 +351,9 @@ class _EsqueciSenhaState extends State<EsqueciSenha> {
 ///
 ///
 /// Tela Cadastro
-/// 
-/// 
-/// 
+///
+///
+///
 
 class TelaCadastro extends StatefulWidget {
   @override
@@ -336,10 +361,164 @@ class TelaCadastro extends StatefulWidget {
 }
 
 class _TelaCadastroState extends State<TelaCadastro> {
+  final formKey = GlobalKey<FormState>();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  String _signEmail;
+  String _signPassword;
+  String _signPasswordConfirm;
+
+  void _submitCadastro() {
+    final form = formKey.currentState;
+
+    if (form.validate()) {
+      form.save();
+      if (_signPassword == _signPasswordConfirm) {
+        _cadastroComando();
+      } else {
+        final snackbarErro = SnackBar(
+          content: Text(
+              'Erro: Senhas "$_signPassword" e "$_signPasswordConfirm" não conferem'),
+        );
+
+        scaffoldKey.currentState.showSnackBar(snackbarErro);
+      }
+    }
+  }
+
+  void _cadastroComando() {
+    final snackbar = SnackBar(
+      content: Text('Email: $_signEmail, password: $_signPassword'),
+    );
+
+    scaffoldKey.currentState.showSnackBar(snackbar);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        key: scaffoldKey,
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.fromLTRB(40, 60, 40, 40),
+          child: Column(
+            children: <Widget>[
+              Row(
+                //cabecalho
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      // coluna 1/3
+                      FlatButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => MyHomePage()));
+                        },
+                        child: Icon(Icons.arrow_back, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      //coluna 2/3
+                      Image.asset('images/logo.png', width: 120.0),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      //coluna 3/3
+                    ],
+                  )
+                ],
+              ),
+              Divider(
+                color: Color(0xFF333333),
+                height: 140,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Crie sua conta",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 28.0,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
+              Divider(color: Color(0xFF33333), height: 40.0),
+              Form(
+                  key: formKey,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                          onSaved: (val) => _signEmail = val,
+                          validator: (val) =>
+                              !EmailValidator.validate(val, true)
+                                  ? 'Email inválido.'
+                                  : null,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                              icon: Icon(
+                                Icons.email,
+                                color: Colors.white,
+                              ),
+                              hintText: 'Insira o seu email',
+                              labelText: 'Endereço de email')),
+                      TextFormField(
+                          validator: (val) =>
+                              val.length < 4 ? 'Senha inválida' : null,
+                          onSaved: (val) => _signPassword = val,
+                          obscureText: true,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                              icon: Icon(
+                                Icons.lock_outline,
+                                color: Colors.white,
+                              ),
+                              hintText: 'Insira sua senha',
+                              labelText: 'Senha')),
+                      TextFormField(
+                          validator: (val) =>
+                              val.length < 4 ? 'Senha inválida' : null,
+                          onSaved: (val) => _signPasswordConfirm = val,
+                          obscureText: true,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                              icon: Icon(
+                                Icons.lock_open,
+                                color: Colors.white,
+                              ),
+                              hintText: 'Repita a senha',
+                              labelText: 'Confirmar senha')),
+                      Divider(color: Color(0xFF33333), height: 40.0),
+                      ConstrainedBox(
+                          constraints:
+                              const BoxConstraints(minWidth: double.infinity),
+                          child: RaisedButton(
+                              shape: new RoundedRectangleBorder(
+                                  borderRadius:
+                                      new BorderRadius.circular(30.0)),
+                              padding:
+                                  EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
+                              onPressed: () {
+                                _submitCadastro();
+                              },
+                              color: Colors.white,
+                              child: Text(
+                                'Enviar',
+                                style: TextStyle(
+                                  fontSize: 17.0,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )))
+                    ],
+                  ))
+            ],
+          ),
+        ));
   }
 }
